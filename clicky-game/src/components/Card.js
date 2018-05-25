63fade299ee49d5a7bb36113aa6ba0a836ddbe23
Victoria League <vleague2@gmail.com>
2018-05-25 10:05:49 -0400

    first game works...

--- clicky-game/src/components/Card.js
+++ clicky-game/src/components/Card.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = props => (
-    <div className="col-md-3">
-        <div className="card mb-5">
-            <img className="card-img-top" src={props.src} alt={props.alt} onClick={() => props.onClick(props.id)} id={props.id}/>
-        </div>
-    </div>
-)
+class Card extends Component {
+    constructor (props) {
+        super(props);
+    }
+
+    state = {
+        count: 1
+    }
+
+    checkGame = () => {
+
+        if (this.props.score === 1) {
+            this.setState({count: 1});
+            this.handleClick()
+        }
+        else {
+            this.handleClick();
+        }
+    }
+
+    handleClick = () => {
+        this.props.onClick();
+
+        if (this.state.count > 1) {
+            this.props.endGame();
+            this.setState({score: 1});
+            console.log("Ending game!")
+        }
+        else {
+            this.setState({ count: this.state.count + 1 });
+            console.log("Current clicks: " + this.state.count)
+        }
+    }
+
+    render() {
+        return(
+            <div className="col-md-3">
+                <div className="card mb-5">
+                    <img className="card-img-top" src={this.props.src} alt={this.props.alt} onClick={this.checkGame} id={this.props.id}/>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Card;
--- clicky-game/src/components/CardContainer.js
+++ clicky-game/src/components/CardContainer.js
@@ -10,64 +10,33 @@ class CardContainer extends Component {
     }
 
     state = {
+        score: 1,
         count: 1,
         pusheens: Pusheens,
-        selected: []
     };
 
-    handleClick = (id) => {
+    handleClick = () => {
         this.setState({ pusheens: shuffle(this.state.pusheens)});
-        if (this.state.selected.length < 1) {
-            this.addToSelected(id);
-            console.log("Adding " + id + " to selected array");
-        }
-        else {
-            this.checkSelected(id);
-        }
+        this.setState({score: this.state.score + 1});
+        console.log("Shuffling Pusheens");
+        this.props.updateCurrentScore(this.state.score);
+        console.log("Score: " + this.state.score);
     }
 
-    changeCount = () => {
-        this.setState({ count: this.state.count + 1 });
-        console.log("Current clicks: " + this.state.count)
-        this.props.updateCurrentScore(this.state.count);
-    }
-
-    checkSelected = (id) => {
-        this.state.selected.map(pusheen => {
-            if (pusheen.id === id) {
-                this.endGame();
-            }
-
-            else {
-                this.addToSelected(id);
-                console.log("Adding " + id + " to selected array")
-            }
-        })
-    }
-    
-    addToSelected = (id) => {
-        this.state.pusheens.map(pusheen => {
-            if (pusheen.id === id) {
-                this.changeCount();
-                this.state.selected.push(pusheen);
-                this.setState({selected: this.state.selected});
-                console.log(this.state.selected);
-            }
-        })
-    }
 
     endGame = () => {
         console.log("End!"); 
-        this.props.updateTopScore(this.state.count); 
-        this.setState({count: 1, selected: []});
-        this.props.updateCurrentScore(this.state.count);
+        this.props.updateTopScore(this.state.score); 
+        this.setState({score: 1});
+        this.props.updateCurrentScore(this.state.score - 1);
+        this.render();
     }
 
     render() {
         return (
             <div className="container" id="card-container">
                 <div className="row">
-                    {Pusheens.map(pusheen => <Card src={pusheen.image} key={pusheen.id} id={pusheen.id} alt={pusheen.name} onClick={this.handleClick}/>)}
+                    {Pusheens.map(pusheen => <Card src={pusheen.image} key={pusheen.id} id={pusheen.id} alt={pusheen.name} endGame={this.endGame} onClick={this.handleClick} score={this.state.score} count="this.state.count"/>)}
                 </div>
             </div>
         );
